9053aafae1a6ac9af3152b0cbe6e592adf8e3646
kaytso <139808215+kaytso@users.noreply.github.com>
2023-07-17 22:28:05 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicframeapp;
+import za.ac.tut.ui.MyFirstFrame;
+/**
+ *
+ * @author Koketso
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // declare variables
+        MyFirstFrame mff;
+        //instantiate the frame
+        mff = new MyFirstFrame();
+    }
+    
+}
